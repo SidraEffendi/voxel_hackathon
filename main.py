764c65ea195f9844c004f2cf8b49285c7d18fed5
Sidra Effendi <sidraeffendi@gmail.com>
2025-03-21 14:37:56 -0400

    first commit
    idea of the project - have a chatbot which detect crime happening and communicates that as an assistant to the person. Act as a chatbot to answer further queries.

---
+++ main.py
@@ -0,0 +1,13 @@
+# detect if there is an attack around the car where the old people are
+# if there is an attack, stop the car
+
+#explain to old person that there is an attack
+
+#use LLaVA Model Card for classification of the attack type https://huggingface.co/liuhaotian/llava-v1.5-7b
+#https://github.com/robert-mcdermott/LLM-Image-Classification
+#use UCF crime dataset to train the model https://www.kaggle.com/datasets/odins0n/ucf-crime-dataset/data
+
+# use the output of the image description to do the classification.
+
+# use the camera to record and analyze the video
+
